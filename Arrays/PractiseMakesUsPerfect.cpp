941a4693dddc5acb7dc157d2e6223d890fb9ae7f
Abhinav Saxena <abhinav150601@gmail.com>
2023-06-12 16:49:14 +0530

    Day 7

---
+++ Arrays/PractiseMakesUsPerfect.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int count=0;
+	int arr[4];
+    for(int i=0;i<4;i++){
+        cin >> arr[i];
+    }
+    for(int i=0;i<4;i++){
+        if(arr[i]>=10){
+            count++;
+        }
+        
+    }
+    cout << count;
+     
+	return 0;
+}
